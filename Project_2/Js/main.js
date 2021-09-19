2aeb53a6049be76198824d6ba579dfb1441ac45b
Mokhtari Hadj Mohamed <75529296+MokhtariHadjMohamed@users.noreply.github.com>
2021-09-19 11:35:45 +0100

    New update 09/19/21

---
+++ Project_2/Js/main.js
@@ -0,0 +1,13 @@
+// Start Menu
+function menu(){
+    let menu = document.getElementById("menu_div")
+
+    menu.style.display = "block"
+}
+
+function outMenu(){
+    let menu = document.getElementById("menu_div")
+
+    menu.style.display = "none"
+}
+// End Menu
